eb67eb2718aa6e3e9d9c99f609c6c33feb3340bd
Owen Adrian <akrivus@gmail.com>
2024-04-10 19:37:22 -0700

    Fixed attribute error.

--- ToolCalling/ToolParams/NumberParamAttribute.cs
+++ ToolCalling/ToolParams/NumberParamAttribute.cs
@@ -3,8 +3,13 @@
 [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
 public class NumberParamAttribute : ToolParamAttribute
 {
-    public NumberParamAttribute(string name, string description, bool required = true, float min = float.MinValue, float max = float.MaxValue, float? multipleOf = null) : base(ParameterType.Number, name, description, required)
+    public NumberParamAttribute(string name, string description, bool required = true, float min = float.MinValue, float max = float.MaxValue) : base(ParameterType.Number, name, description, required)
     {
-        Range = new Range(min, max, multipleOf);
+        Range = new Range(min, max);
+    }
+
+    public NumberParamAttribute(string name, string description, bool required = true, float min = float.MinValue, float max = float.MaxValue, float mutlipleOf = 1) : base(ParameterType.Number, name, description, required)
+    {
+        Range = new Range(min, max, mutlipleOf);
     }
 }
